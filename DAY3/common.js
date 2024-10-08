16ef7a3e373c92bb7a09ddc8a472a8a707294920
HA <haagarwal67@gmail.com>
2024-10-08 19:58:48 +0530

    first commit

---
+++ DAY3/common.js
@@ -0,0 +1,2 @@
+const check=require("/Volumes/HARSH/FSD___/FSD_CE/DAY3/re.js");
+console.log("Multiply is: ",check.multiply(5,10));
---
+++ DAY3/req.js
@@ -0,0 +1,8 @@
+import { add, subtract, PI } from "/Volumes/HARSH/FSD___/FSD_CE/DAY3/rem.js";
+import * as hello from "/Volumes/HARSH/FSD___/FSD_CE/DAY3/rem.js";
+import multiply from "/Volumes/HARSH/FSD___/FSD_CE/DAY3/rem.js";
+export {add,subtract,PI,multiply};
+console.log("Add: " + hello.add(13, 5));        
+console.log("Subtract: " + hello.subtract(13, 5)); 
+console.log("PI: " + hello.PI);                  
+console.log("Multiply: " + multiply(13, 5));     
