d57f18f7bfae7436bd7c1a295b4bd4b7ec8c4892
Harsh Agarwal <130899605+agrwalh@users.noreply.github.com>
2024-12-13 12:48:36 +0530

    Update App.jsx

--- REACT/vite/vite-project/src/App.jsx
+++ REACT/vite/vite-project/src/App.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./components/MainLayout";
-import Login from "./components/Login";
-import Registration from "./components/Registration";
-import Dashboard from "./components/Dashboard";
+// import React, { useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import MainLayout from "./components/MainLayout";
+// import Login from "./components/Login";
+// import Registration from "./components/Registration";
+// import Dashboard from "./components/Dashboard";
+
+// function App() {
+//   const [store, setStore] = useState({email: "agarwal22041@gmail.com", pwd: "23" }); 
+
+//   return (
+//     <div>
+      
+//       <div>
+//         <h3>Stored Data:</h3>
+//         <pre>{JSON.stringify(store, null, 2)}</pre>
+//       </div>
+
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<MainLayout />}>
+//             <Route path="login" element={<Login regDataLogin={store} />} />
+//             <Route path="registration" element={<Registration regData={setStore} />} />
+//             <Route path="dashboard" element={<Dashboard />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import MovieList from "/Volumes/HARSH/REACT/Movie.jsx"; 
+import "/Volumes/HARSH/REACT/Movies.css";          
 
 function App() {
-  const [store, setStore] = useState({email: "agarwal22041@gmail.com", pwd: "23" }); 
-
   return (
     <div>
-      
-      <div>
-        <h3>Stored Data:</h3>
-        <pre>{JSON.stringify(store, null, 2)}</pre>
-      </div>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route path="login" element={<Login regDataLogin={store} />} />
-            <Route path="registration" element={<Registration regData={setStore} />} />
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <h1>Movie Gallery</h1>
+      <MovieList />
     </div>
   );
 }
